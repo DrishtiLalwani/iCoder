04423b42927402a5bc33378d59e15c9923f62752
drishti <drishtilalwani1432@gmail.com>
2024-02-08 18:44:44 +0530

    updated

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    s_no = models.AutoField(primary_key=True)
+    name = models.CharField (max_length=50)
+    email = models.EmailField()
+    phone = models.CharField (max_length=13)
+    content = models.TextField()
+
+    def __str__(self):
+        return "message from "+ self.name
+
+
--- home/views.py
+++ home/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render, HttpResponse
-
+from home.models import Contact
+from django.contrib import messages
 # Create your views here.
 def home(request): 
-    return HttpResponse('This is home ')
-
-def contact(request):
-    return HttpResponse("This is contact")
+    return render(request, 'home/home.html')
 
 def about(request): 
-    return HttpResponse('This is about')
+    return render(request, 'home/about.html')
+
+def contact(request):
+    
+    if request.method=='POST':
+        name= request.POST['name']
+        email= request.POST['email']
+        phone= request.POST['phone']
+        content=request.POST['content']
+
+        if len(name)<2 or len(email)<5 or len(phone)<10 or len(content)<5:
+            messages.warning (request, "jo bhi error ho")
+        else:
+            contact=Contact(name=name,email=email,phone=phone,content=content)
+            contact.save()
+  
+
+    return render(request, 'home/contact.html')
